1b4a3d06257b60a71dc65edbf7239192d4c69e61
J-Xander <j.xanderoficial@gmail.com>
2024-08-21 20:03:45 -0600

    Moviendo los modulos de Calificacion a Academico

---
+++ GestorDeNotas/Academico/models.py
@@ -0,0 +1,74 @@
+from django.db import models
+import datetime
+
+# Create your models here.
+from Usuario.models import Maestro
+
+class Asignatura(models.Model):
+    """Representa las clases que se imparten"""
+
+    nombre = models.CharField('Nombre de la clase', max_length=30, unique=True)
+    codigo = models.CharField('Codigo de la clase', max_length=5, unique=True)
+
+    class Meta:
+        db_table = 'Asignaturas'
+
+    # ------ METODOS ------
+
+# ----------------------------------
+
+class Bimestre(models.Model):
+    """Representa el bimestre actual"""
+
+    # ---- Semestres ----
+    BIMESTRE_CHOICE = (
+        ('I', 'Primer semestre'),
+        ('II', 'Segundo bimestre'),
+    )
+    # -----------------------------
+
+    anio = models.PositiveIntegerField(default=datetime.date.today().year, editable=False)
+    n_bimestre = models.CharField(
+        'bimestre',
+        choices=BIMESTRE_CHOICE,
+        max_length=2,
+    )
+
+    class Meta:
+        db_table='Bimestres'
+        constraints = [
+            models.UniqueConstraint(fields=['anio', 'n_bimestre'], name='No pueden haber dos semestres iguales en un mismo año')
+        ]
+
+    # ------- METODOS ------
+    def __str__(self):
+        return f'{self.n_bimestre} {self.anio}'
+    
+# ------------------------------------
+
+class PlanEstudio(models.Model):
+    """Representa las clases que impartira el docente en x semestre"""
+
+    maestro = models.ForeignKey(
+        Maestro,
+        on_delete=models.CASCADE,
+        related_name='plan'
+    )
+    clase = models.ForeignKey(
+        Asignatura,
+        on_delete=models.CASCADE,
+        related_name='plan'
+    )
+
+    class Meta:
+        db_table = 'Planes'
+        constraints = [
+            models.UniqueConstraint(fields=['maestro', 'clase'], name='Un maestro no puede llevar clases repetidas')
+        ]
+
+    # ------ METODOS -------
+
+    def __str__(self):
+        return f'{self.maestro} enseña {self.clase}'
+    
+# ----------------------------------
