d60f88017df0f64e970bc7111d24999c23bf503d
J-Xander <j.xanderoficial@gmail.com>
2024-08-22 01:14:31 -0600

    Añadiendo las tablas al registro admin

--- GestorDeNotas/Academico/admin.py
+++ GestorDeNotas/Academico/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Asignatura, Bimestre, PlanEstudio
+
+@admin.register(Asignatura)
+class AsignaturaAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Bimestre)
+class BimestreAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(PlanEstudio)
+class PlanEstudioAdmin(admin.ModelAdmin):
+    pass
--- GestorDeNotas/Calificacion/admin.py
+++ GestorDeNotas/Calificacion/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Evaluacion, Calificacion
+
+@admin.register(Evaluacion)
+class EvaluacionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Calificacion)
+class CalificacionAdmin(admin.ModelAdmin):
+    pass
